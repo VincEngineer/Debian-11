926b881aebdc21a6b42ed1bdae7aa502866c65e7
Vincenzo Bonaiuto <105960233+VincEngineer@users.noreply.github.com>
2023-04-29 20:18:14 +0100

    Update web-server-automation.sh

--- web-server-automation.sh
+++ web-server-automation.sh
@@ -2,20 +2,19 @@
 # This readme is made with the purpose of automating the web server installation and save your time in future occasions
 
 # Debian 11 (HOST) Essential Installations:
-apt-get update
-apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-apt-get install sudo
-apt-get install ufw
-sudo apt install curl
-sudo apt install git
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.deb
-sudo apt install ./rclone-current-linux-amd64.deb || { echo -e "\e[31mError installing rclone. Exiting.\e[0m"; exit 1; }
+yes | apt-get update
+yes | apt-get upgrade -y
+yes | sudo apt-get dist-upgrade -y
+yes | apt-get install sudo
+yes | apt-get install ufw
+yes | sudo apt install curl
+yes | sudo apt install git
+yes | curl -O https://downloads.rclone.org/rclone-current-linux-amd64.deb
+yes | sudo apt install ./rclone-current-linux-amd64.deb || { echo -e "\e[31mError installing rclone. Exiting.\e[0m"; exit 1; }
 
 # Prompt the user for their MEGA email and password
 echo -e "\e[33mDo you want to enter your Mega credentials? [Y/N]:\e[0m"
 read mega_choice
-
 if [[ $mega_choice =~ ^[Yy]$ ]]; then
 echo -e "\e[33 Visit https://mega.nz/ and create and account to automatically backup the database(You can Ignore this step)\e[0m"
 echo -e "\e[33mEnter your Mega email address:\e[0m"
@@ -40,12 +39,12 @@ user = $mega_email
 pass = $password
 EOL
 
-sudo apt-get install net-tools
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw allow from 80.233.0.0/16 to any port 22 proto tcp
-sudo ufw allow from 80.233.0.0/16 to 172.18.0.2 port 5432 proto tcp
-sudo ufw enable
+yes | apt-get install net-tools
+yes | ufw default deny incoming
+yes | ufw default allow outgoing
+yes | ufw allow from 80.233.0.0/16 to any port 22 proto tcp
+yes | ufw allow from 80.233.0.0/16 to 172.18.0.2 port 5432 proto tcp
+yes | ufw enable
 
 # Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -73,13 +72,11 @@ sudo usermod -aG docker $username
 sudo usermod -aG sudo $username
 
 # Install Docker Compose
-sudo apt-get update
-sudo apt-get install -y curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install megatools
-sudo apt-get install -y megatools
+yes | sudo apt-get install -y megatools
 
 # Download the folder from MEGA
 mkdir IronPentest-WebServer
@@ -88,3 +85,7 @@ megadl "https://mega.nz/folder/OLJTlCwT#fZjlW2dqFuggpVUpgSaqRw" --path ./IronPen
 # Installing Web Server
 cd IronPentest-WebServer
 docker-compose build --no-cache && docker-compose up -d --no-recreate
+
+rm rclone-current-linux-amd64.deb
+rm web-server-automation.sh
+rm get-docker.sh
